28ac3187936fa08c3973d731bf48b2ac06fd0384
m29dev <michalmajchrzak.it@gmail.com>
2023-10-11 16:54:34 +0200

    search page query params added

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,11 +1,12 @@
 import Navbar from '../components/navbar'
 import { Form } from 'react-bootstrap'
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useSearchQueryMutation } from '../services/musicService'
 import { useSelector } from 'react-redux'
 import SearchResultTracks from '../components/searchResultTracks'
 import SearchResultArtists from '../components/searchResultArtists'
 import SearchResultAlbums from '../components/searchResultAlbums'
+import { useSearchParams } from 'react-router-dom'
 
 const Search = () => {
     const { authInfo } = useSelector((state) => state.auth)
@@ -17,29 +18,67 @@ const Search = () => {
     const [playlists, setPlaylists] = useState(null)
     const [albums, setAlbums] = useState(null)
 
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const getSearchResults = useCallback(
+        async (query) => {
+            try {
+                console.log('gsr')
+                // GET SEARCH RESULTS
+                const res = await getSearchQuery({
+                    access_token: authInfo?.access_token,
+                    query,
+                }).unwrap()
+
+                setTracks(res?.tracks?.items)
+                setArtists(res?.artists?.items)
+                setPlaylists(res?.playlists?.items)
+                setAlbums(res?.albums?.items)
+            } catch (err) {
+                console.log(err)
+            }
+        },
+        [
+            getSearchQuery,
+            authInfo,
+            setTracks,
+            setArtists,
+            setPlaylists,
+            setAlbums,
+        ]
+    )
+
     const onSearchQuery = async (query) => {
         try {
-            if (query === '')
-                return (
-                    setTracks(null),
+            if (query === '') {
+                setTracks(null),
                     setArtists(null),
                     setPlaylists(null),
                     setAlbums(null)
-                )
+            }
 
-            const res = await getSearchQuery({
-                access_token: authInfo?.access_token,
-                query,
-            }).unwrap()
-
-            setTracks(res?.tracks?.items)
-            setArtists(res?.artists?.items)
-            setPlaylists(res?.playlists?.items)
-            setAlbums(res?.albums?.items)
+            // SET SEARCH QUERIES IN THE URL
+            setSearchParams(`?${new URLSearchParams({ q: query })}`)
         } catch (err) {
             console.log(err)
         }
     }
+
+    // on searchParams update GET SEARCH RESULTS
+    useEffect(() => {
+        console.log('searchin from UF: ', searchParams?.get('q'))
+        if (searchParams?.get('q') === '') return
+        getSearchResults(searchParams?.get('q'))
+    }, [searchParams, getSearchResults])
+
+    // on init set search value same as in searchParams if exists
+    useEffect(() => {
+        console.log('on init: set serach value')
+        if (searchParams?.get('q')) {
+            setSearchQuery(searchParams?.get('q'))
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <div className="center-box">

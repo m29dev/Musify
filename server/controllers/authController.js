d3fc552ae43c7f783b85dec8ddb00c502f2ad494
m29dev <michalmajchrzak.it@gmail.com>
2023-10-12 21:56:30 +0200

    auth fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -12,16 +12,7 @@ const sign_in = async (req, res) => {
             playlist-read-private
             playlist-modify-public`
 
-        res.json(
-            'https://accounts.spotify.com/authorize?' +
-                querystring.stringify({
-                    response_type: 'code',
-                    client_id: process.env.SPOTIFY_CLIENT_ID,
-                    scope: scope,
-                    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-                    state: 1234567890123456,
-                })
-        )
+        res.redirect('https://google.com')
 
         // res.redirect(
         //     'https://accounts.spotify.com/authorize?' +

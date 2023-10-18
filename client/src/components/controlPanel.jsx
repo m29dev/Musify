861b6659c20ba7d90e6301ba9e953c4bb39c6fc0
m29dev <michalmajchrzak.it@gmail.com>
2023-10-18 11:45:35 +0200

    mobile screens layout fix

--- client/src/components/controlPanel.jsx
+++ client/src/components/controlPanel.jsx
@@ -107,81 +107,193 @@ const ControlPanel = () => {
         <>
             {/* CONTROL PANEL */}
             {songInfo && authInfo && (
-                <div className="control-panel-box-absolute">
-                    {/* left bar */}
-                    <div className="control-panel-left">
-                        {/* title */}
-                        {songInfo?.spotify_song && (
-                            <>
-                                <div className="control-panel-title">
-                                    <h1>{songInfo?.spotify_song?.name}</h1>
-                                    <div className="title-artists-box">
-                                        {songInfo?.spotify_song?.artists.map(
-                                            (artist, index) => {
-                                                return (
-                                                    <h4
-                                                        key={artist?.name}
-                                                        onClick={() => {
-                                                            navigate(
-                                                                `/artists/${artist?.id}`
-                                                            )
-                                                        }}
-                                                        className="artist-link"
-                                                    >
-                                                        {index > 0
-                                                            ? `, ${artist?.name}`
-                                                            : `${artist?.name}`}
-                                                    </h4>
-                                                )
-                                            }
-                                        )}
+                <>
+                    <div className="control-panel-box-absolute">
+                        {/* left bar */}
+                        <div className="control-panel-left">
+                            {/* title */}
+                            {songInfo?.spotify_song && (
+                                <>
+                                    <div className="control-panel-title">
+                                        <h1>{songInfo?.spotify_song?.name}</h1>
+                                        <div className="title-artists-box">
+                                            {songInfo?.spotify_song?.artists.map(
+                                                (artist, index) => {
+                                                    return (
+                                                        <h4
+                                                            key={artist?.name}
+                                                            onClick={() => {
+                                                                navigate(
+                                                                    `/artists/${artist?.id}`
+                                                                )
+                                                            }}
+                                                            className="artist-link"
+                                                        >
+                                                            {index > 0
+                                                                ? `, ${artist?.name}`
+                                                                : `${artist?.name}`}
+                                                        </h4>
+                                                    )
+                                                }
+                                            )}
+                                        </div>
                                     </div>
-                                </div>
-                            </>
-                        )}
-                    </div>
-
-                    {/* center bar */}
-                    <div className="control-panel-center">
-                        {/* upper bar */}
-                        <div className="control-panel-btn-box">
-                            <BiSkipPrevious
-                                className="control-panel-btn"
-                                onClick={() => {
-                                    changeSong('previous')
-                                }}
-                            ></BiSkipPrevious>
-
-                            <BsFillPlayCircleFill
-                                onClick={() => {
-                                    updatePlayVideo(true)
-                                }}
-                                className="control-panel-btn"
-                            ></BsFillPlayCircleFill>
-
-                            <BsFillPauseCircleFill
-                                onClick={() => {
-                                    updatePlayVideo(false)
-                                }}
-                                className="control-panel-btn"
-                            ></BsFillPauseCircleFill>
-
-                            <BiSkipNext
-                                className="control-panel-btn"
-                                onClick={() => {
-                                    changeSong('next')
-                                }}
-                            ></BiSkipNext>
+                                </>
+                            )}
                         </div>
 
-                        {/* lower bar */}
-                        <div className="control-panel-duration-box">
-                            <div className="control-panel-current-duration">
-                                {fancyTimeFormat(
-                                    controlPanelInfo?.durationVideo
-                                )}
+                        {/* center bar */}
+                        <div className="control-panel-center">
+                            {/* upper bar */}
+                            <div className="control-panel-btn-box">
+                                <BiSkipPrevious
+                                    className="control-panel-btn"
+                                    onClick={() => {
+                                        changeSong('previous')
+                                    }}
+                                ></BiSkipPrevious>
+
+                                <BsFillPlayCircleFill
+                                    onClick={() => {
+                                        updatePlayVideo(true)
+                                    }}
+                                    className="control-panel-btn"
+                                ></BsFillPlayCircleFill>
+
+                                <BsFillPauseCircleFill
+                                    onClick={() => {
+                                        updatePlayVideo(false)
+                                    }}
+                                    className="control-panel-btn"
+                                ></BsFillPauseCircleFill>
+
+                                <BiSkipNext
+                                    className="control-panel-btn"
+                                    onClick={() => {
+                                        changeSong('next')
+                                    }}
+                                ></BiSkipNext>
                             </div>
 
+                            {/* lower bar */}
+                            <div className="control-panel-duration-box">
+                                <div className="control-panel-current-duration">
+                                    {fancyTimeFormat(
+                                        controlPanelInfo?.durationVideo
+                                    )}
+                                </div>
+
+                                <input
+                                    type="range"
+                                    min={0}
+                                    max={
+                                        songInfo?.spotify_song?.duration_ms
+                                            ? songInfo?.spotify_song
+                                                  ?.duration_ms / 1000
+                                            : ''
+                                    }
+                                    step={0.01}
+                                    value={duration}
+                                    className="control-panel-range"
+                                    onChange={(event) => {
+                                        changeDuration(event)
+                                    }}
+                                />
+                                <div className="control-panel-duration">
+                                    {fancyTimeFormat(
+                                        songInfo?.spotify_song?.duration_ms /
+                                            1000
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* right bar */}
+                        <div className="control-panel-right">
+                            <div className="control-panel-volume-box">
+                                <BsFillVolumeDownFill className="control-panel-btn"></BsFillVolumeDownFill>
+                                <input
+                                    type="range"
+                                    min={0}
+                                    max={1}
+                                    step={0.02}
+                                    value={controlPanelInfo?.volumeVideo}
+                                    onChange={(event) => {
+                                        updateVolume(event.target.valueAsNumber)
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* mobile screen version */}
+                    <div className="control-panel-box-mobile">
+                        {/* top box - song info (img, title), control btns */}
+                        <div className="control-panel-box-mobile-top">
+                            {/* title */}
+                            {songInfo?.spotify_song && (
+                                <>
+                                    <div className="control-panel-title">
+                                        <h1>{songInfo?.spotify_song?.name}</h1>
+                                        <div className="title-artists-box">
+                                            {songInfo?.spotify_song?.artists.map(
+                                                (artist, index) => {
+                                                    return (
+                                                        <h4
+                                                            key={artist?.name}
+                                                            onClick={() => {
+                                                                navigate(
+                                                                    `/artists/${artist?.id}`
+                                                                )
+                                                            }}
+                                                            className="artist-link"
+                                                        >
+                                                            {index > 0
+                                                                ? `, ${artist?.name}`
+                                                                : `${artist?.name}`}
+                                                        </h4>
+                                                    )
+                                                }
+                                            )}
+                                        </div>
+                                    </div>
+                                </>
+                            )}
+
+                            {/* control btns */}
+                            <div className="control-panel-btn-box">
+                                <BiSkipPrevious
+                                    className="control-panel-btn"
+                                    onClick={() => {
+                                        changeSong('previous')
+                                    }}
+                                ></BiSkipPrevious>
+
+                                <BsFillPlayCircleFill
+                                    onClick={() => {
+                                        updatePlayVideo(true)
+                                    }}
+                                    className="control-panel-btn"
+                                ></BsFillPlayCircleFill>
+
+                                <BsFillPauseCircleFill
+                                    onClick={() => {
+                                        updatePlayVideo(false)
+                                    }}
+                                    className="control-panel-btn"
+                                ></BsFillPauseCircleFill>
+
+                                <BiSkipNext
+                                    className="control-panel-btn"
+                                    onClick={() => {
+                                        changeSong('next')
+                                    }}
+                                ></BiSkipNext>
+                            </div>
+                        </div>
+
+                        {/* bottom box - progress input */}
+                        <div className="control-panel-box-mobile-bottom">
                             <input
                                 type="range"
                                 min={0}
@@ -198,31 +310,9 @@ const ControlPanel = () => {
                                     changeDuration(event)
                                 }}
                             />
-                            <div className="control-panel-duration">
-                                {fancyTimeFormat(
-                                    songInfo?.spotify_song?.duration_ms / 1000
-                                )}
-                            </div>
                         </div>
                     </div>
-
-                    {/* right bar */}
-                    <div className="control-panel-right">
-                        <div className="control-panel-volume-box">
-                            <BsFillVolumeDownFill className="control-panel-btn"></BsFillVolumeDownFill>
-                            <input
-                                type="range"
-                                min={0}
-                                max={1}
-                                step={0.02}
-                                value={controlPanelInfo?.volumeVideo}
-                                onChange={(event) => {
-                                    updateVolume(event.target.valueAsNumber)
-                                }}
-                            />
-                        </div>
-                    </div>
-                </div>
+                </>
             )}
         </>
     )
